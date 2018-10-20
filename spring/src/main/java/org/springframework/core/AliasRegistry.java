40679da44b517b929754953d90efb1348c98fe78
= <383961310@qq.com>
2018-10-20 11:57:02 +0800

    org.springframework.core.AliasRegistry 注释

--- spring/src/main/java/org/springframework/core/AliasRegistry.java
+++ spring/src/main/java/org/springframework/core/AliasRegistry.java
@@ -20,6 +20,8 @@ package org.springframework.core;
  * Common interface for managing aliases. Serves as super-interface for
  * {@link org.springframework.beans.factory.support.BeanDefinitionRegistry}.
  *
+ * 管理别名的通用接口,作为{@link org.springframework.beans.factory.support.BeanDefinitionRegistry}
+ * 接口的父接口
  * @author Juergen Hoeller
  * @since 2.5.2
  */
@@ -27,6 +29,9 @@ public interface AliasRegistry {
 
     /**
      * Given a name, register an alias for it.
+     *
+     * 给一个名称注册一个别名
+     *
      * @param name the canonical name
      * @param alias the alias to be registered
      * @throws IllegalStateException if the alias is already in use
@@ -36,6 +41,9 @@ public interface AliasRegistry {
 
     /**
      * Remove the specified alias from this registry.
+     *
+     * 移除注册的指定别名
+     *
      * @param alias the alias to remove
      * @throws IllegalStateException if no such alias was found
      */
@@ -44,6 +52,8 @@ public interface AliasRegistry {
     /**
      * Determine whether this given name is defines as an alias
      * (as opposed to the name of an actually registered component).
+     * 判断给定的名称是不是定义的别名
+     *
      * @param beanName the bean name to check
      * @return whether the given name is an alias
      */
@@ -51,6 +61,9 @@ public interface AliasRegistry {
 
     /**
      * Return the aliases for the given name, if defined.
+     *
+     * 返回给定名称的别名,若果存在的话
+     *
      * @param name the name to check for aliases
      * @return the aliases, or an empty array if none
      */
